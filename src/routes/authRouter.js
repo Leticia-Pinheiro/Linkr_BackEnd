66df122b1cba81f814ee6ef4ba05080e628b5a9f
williameiji <william.fujimori@gmail.com>
2022-08-09 16:25:45 -0300

    feat: route login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,9 +3,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/schemasValidator.js";
 import signupSchema from "../schemas/signupSchema.js";
 import { signupUser } from "../controllers/authController.js";
+import signinSchema from "../schemas/signinSchema.js";
+import { signinUser } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema(signupSchema), signupUser);
+authRouter.post("/signin", validateSchema(signinSchema), signinUser);
 
 export default authRouter;
